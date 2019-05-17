10107c9c2877010e46bfbbf0fb650a117806042b
Naman Modi <modi.naman14@gmail.com>
2019-05-17 16:58:27 +0530

    Added Mongoose to app.js file

--- Hackathon/app.js
+++ Hackathon/app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost/codewars", {useNewUrlParser: true}, (err) => {
+  if (err){
+    console.log("error connecting to mongoose")
+  }
+  else {
+    console.log('mongoose connected')
+  }
+});
+mongoose.set('useCreateIndex', true)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
