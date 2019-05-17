8e4d21f7df911736309eebee6b8c8e3a6ff2d20a
Naman Modi <modi.naman14@gmail.com>
2019-05-17 19:13:00 +0530

    Added Login & Registration

--- Hackathon/routes/index.js
+++ Hackathon/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var User = require('../models/user');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login');
-});
+
+
+router.get('/', (req, res , next) => {
+  res.render('login')
+})
+
+router.post("/", function(req, res, next){
+  var { email, password } = req.body;
+  if (!email || !password) {
+      return res.redirect("/users");
+  }
+  User.findOne({ email: email }, (err, user) => {
+      console.log(user)
+      if (err) return next(err);
+      if (!user) {
+          return res.redirect("/users");
+      }
+      if(!user.validatePassword(password)) return res.redirect('/users');
+      // create session
+      req.session.userId = user._id;
+      res.redirect('/users');
+  });
+ });
 
 module.exports = router;
