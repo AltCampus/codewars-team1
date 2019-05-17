9b5065b8f27b9de3e1be27b545e1c5919d5f7870
Naman Modi <modi.naman14@gmail.com>
2019-05-17 17:18:10 +0530

    Added Model for User Schema in Models Folder

---
+++ Hackathon/models/user.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, "Email is Required"],
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    github : {
+        name: String,
+        photo: String,
+    }
+},{timestamps: true});
+
+userSchema.pre('save', function(next) {
+    // hash password using bcrypt.hashing
+    this.password = bcrypt.hashSync(this.password, 10);
+    return next();
+})
+
+userSchema.methods.validatePassword = function(password){
+    // compare password using bcrypt.compareSync
+    return bcrypt.compareSync('password', this.password)
+}
+
+var User = mongoose.model('User', userSchema)
+module.exports = User;
